3aea8ace0e29acb4952acb7999531f05779d08e1
Andreea Simona Oancea <andreea84sim@googlemail.com>
2018-08-04 15:55:25 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -137,14 +137,30 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
-      # Should you want to write your own test for this situation,
-      # read the following code and comments.
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 
-      # Can you copy the syntax of the tests above to write a test for a board
-      # entirely filled with Xs?"
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
 
-      # Define the board with values that should create the desired output
-      # *** Edit the line below ***
+      expect(rows[0]).to eq(" X | X | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" X | X | X ")
+    end
+
+
+      board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" X | O | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | X | O ")
+    end
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
 
       # Don't touch the following lines.
